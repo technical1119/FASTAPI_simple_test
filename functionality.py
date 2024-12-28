e3788028b72b114672b93100ec32f981338898e6
technical1119 <o.galych@prolife.de>
2024-12-28 17:06:35 +0100

    work bitch

--- functionality.py
+++ functionality.py
@@ -10,7 +10,16 @@ def createDriver() -> webdriver.Chrome:
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    
+    service = Service(executable_path='/usr/local/bin/chromedriver')
+    
+    driver = webdriver.Chrome(
+        service=service,
+        options=chrome_options
+    )
     return driver
 
 async def getGoogleHomepage():
