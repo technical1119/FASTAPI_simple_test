03873b33b571451a8e04b758f008332a0cd12c8f
technical1119 <o.galych@prolife.de>
2024-12-28 16:39:44 +0100

    Update functionality.py

--- functionality.py
+++ functionality.py
@@ -10,13 +10,9 @@ def createDriver() -> webdriver.Chrome:
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.binary_location = "/usr/bin/google-chrome"  # Standard Linux Chrome location
-    
-    service = Service('/usr/local/bin/chromedriver')  # Standard Linux ChromeDriver location
     
     try:
         driver = webdriver.Chrome(
-            service=service,
             options=chrome_options
         )
     except Exception as e:
